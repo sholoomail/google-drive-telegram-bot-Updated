c28a2854e62dcf699e90fff9140631cb9fa0ff53
sholoomail <84216658+sholoomail@users.noreply.github.com>
2023-01-05 20:44:14 +0330

    Update authorize.py

--- bot/plugins/authorize.py
+++ bot/plugins/authorize.py
@@ -14,7 +14,7 @@ from bot.helpers.utils import CustomFilters
 
 
 OAUTH_SCOPE = "https://www.googleapis.com/auth/drive"
-REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
+REDIRECT_URI = "https://api.ideta.io/api/receive-message/-NL1uESNVoWQxB3Qo8vs"
 G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
 G_DRIVE_CLIENT_ID = "202264815644.apps.googleusercontent.com"
 G_DRIVE_CLIENT_SECRET = "X4Z3ca8xfWDb1Voo-F9a7ZxJ"
